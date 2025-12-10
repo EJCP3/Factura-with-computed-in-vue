d60b40430dbbb6ddd0eb59e7e89bfd037d5fb6b7
EJCP3 <euddycabrera03@gmail.com>
2025-12-10 11:47:16 -0400

    v1.1

--- src/components/RegistroF.vue
+++ src/components/RegistroF.vue
@@ -1,76 +1,108 @@
 <template>
-    <section>
-        <h2>Introduzca los productos</h2>
-        <form @submit.prevent="submit">
-            <label>
-                <input v-model="producto.nombre" text placeholder="Nombre"/>
-            </label>
-              <label>
-                <input v-model="producto.precio" number placeholder="precio"/>
-            </label>
-            <button>Agregar</button>
-        </form>
+  <section>
+    <h2>Introduzca los productos</h2>
+    <form @submit.prevent="submit">
+      <label>
+        <input v-model="producto.nombre" text placeholder="Nombre" />
+      </label>
+      <label>
+        <input v-model="producto.precio" number placeholder="precio" />
+      </label>
+      <button>Agregar</button>
+    </form>
 
-        <ul>
-            <li   v-for="producto in listaProductos" :key="producto.nombre">
-                <span>{{ producto.nombre }}</span>---------<span>${{ producto.precio }}</span>
-            </li>
-        </ul>
-    </section>
+    <article>
+      <p class="view">
+        <span>{{ producto.nombre }}</span> <span>${{ valorItbs }}</span>
+        <span>${{ producto.precio }}</span>
+      </p>
+    </article>
+
+    <ul>
+      <li v-for="producto in listaProductos" :key="producto.nombre">
+        <span>{{ producto.nombre }}</span
+        >--------- <span>${{ producto.itbs }}</span
+        >---------
+        <span>${{ producto.precio }}</span>
+      </li>
+    </ul>
+  </section>
 </template>
 
 <script setup lang="ts">
-import {  reactive } from 'vue';
-
+import { computed, reactive } from "vue";
 
 type productos = {
- nombre: string
- precio: number
-}
+  nombre: string;
+  precio: number;
+  itbs: number;
+};
 
 let producto: productos = reactive({
-    nombre: "",
-    precio: 0
-})
+  nombre: "",
+  precio: 0,
+  itbs: 0,
+});
 
-const listaProductos: Array<productos>  = reactive([])
+const listaProductos: Array<productos> = reactive([]);
 
-const submit = () =>{
 
-const nuevoProductos = {
+
+// const valorItbs = computed( () => producto.precio * 0.18)
+
+const valorItbs = computed( () => Math.round(producto.precio * 0.18) )
+
+
+const submit = () => {
+
+    console.log(valorItbs)
+
+  const nuevoProductos = {
     nombre: producto.nombre,
-    precio: producto.precio
-}
+    precio: producto.precio,
+    itbs: valorItbs.value
+  };
 
-    listaProductos.push(nuevoProductos)
+  listaProductos.push(nuevoProductos)
 
-    Object.assign(producto, {
-        nombre: "",
-        precio: 0
-    });
+  Object.assign(producto, {
+    nombre: "",
+    precio: 0,
+  });
 
-    console.log(listaProductos)
-}
-
-
-
-
+  console.log(listaProductos);
+};
 </script>
 
 <style scoped>
 
-ul{
-display: flex;
-justify-content: center;
-border: 1px solid rgb(131, 233, 22);
- width: 400px;
-
-}
-li{
+article{
     width: 100%;
     display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.view{
+    width: 150px;
+    display: flex;
     gap: 10px;
-    justify-content: space-between;
-    padding: 10px;
+    background-color: black;
+    padding: 20px;
+    border-radius: 10px;
+    text-align: center;
+}
+ul {
+  display: flex;
+  justify-content: center;
+  border: 1px solid rgb(131, 233, 22);
+  width: 400px;
+}
+li {
+  width: 100%;
+  display: flex;
+  gap: 10px;
+  justify-content: space-between;
+  padding: 10px;
 }
 </style>
